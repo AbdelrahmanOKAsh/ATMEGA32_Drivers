c016914552cfd426ca38cae4fe70a10d8755b644
Abdelrahman Gamal <agmal600@gmail.com>
2026-05-17 21:58:23 +0300

    Modifing [ GPIO v3.0 ]

--- MCAL/GPIO/GPIO v3.0/GPIO_Program.c
+++ MCAL/GPIO/GPIO v3.0/GPIO_Program.c
@@ -30,7 +30,7 @@ VALIDATION GPIO_enumPinInit (const GPIO_PinConfig *PinConfig)
             {
                 case INPUT_FLOATING      : GPIO[PinConfig->PortID].DDR  &= ~(1 << PinConfig->PinID); GPIO[PinConfig->PortID].PORT &= ~(1 << PinConfig->PinID);                       break;
                 case OUTPUT              : GPIO[PinConfig->PortID].DDR  |=  (1 << PinConfig->PinID);                                                                                 break;
-                case INPUT_INTERNALPULLUP: GPIO[PinConfig->PortID].DDR  &= ~(1 << PinConfig->PinID); GPIO[PinConfig->PortID].DDR  |=  (1 << PinConfig->PinID); SFIO &= ~(1 << PUPD); break; 
+                case INPUT_INTERNALPULLUP: GPIO[PinConfig->PortID].DDR  &= ~(1 << PinConfig->PinID); GPIO[PinConfig->PortID].PORT  |=  (1 << PinConfig->PinID); SFIO &= ~(1 << PUPD); break; 
                 default                  : Local_enumValidationStatus = INVALID;                                                                                                     break;
             }
         }
